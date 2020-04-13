d7e15b93e0f6adac22ec9f75b02fb0b43abdb90c
deepak rana <deepakrana@ieee.org>
2020-04-13 16:43:53 +0530

    Add project files.

---
+++ ThiefHelper/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace ThiefHelper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::ThiefHelper.Properties.Resources._5e9082141a22c684351274;
+            this.pictureBox1.Location = new System.Drawing.Point(590, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(198, 203);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(25, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(204, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Enter Capacity of your Bag: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(25, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(179, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Items available to steal: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(25, 87);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(166, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Weight of items in list:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(25, 151);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(216, 20);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Profit of items (respectively):";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(275, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(275, 52);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(275, 89);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(254, 58);
+            this.textBox3.TabIndex = 7;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(275, 153);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(254, 83);
+            this.textBox4.TabIndex = 8;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(29, 246);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(225, 140);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Can take fractional things?";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(7, 28);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(52, 24);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Yes";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(7, 59);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(47, 24);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "No";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button1.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(300, 305);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(166, 39);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Calculate!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            
+            this.Name = "Form1";
+            this.Text = "Thief Helper";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ ThiefHelper/Form1.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ThiefHelper
+{
+
+    public class KnapsackDP
+    {
+        int knapsack_weight;
+        int numberOfItems, totalValue;
+        int[] weight;
+        int[] value;
+        int[,] memoTable;
+ 
+        public KnapsackDP(int knapsack_weight1, int N, int[] weight1, int[] value1)
+        {
+            this.numberOfItems = N;
+            this.totalValue = 0;
+            this.knapsack_weight = knapsack_weight1;
+            this.weight = weight1;
+            this.value = value1;
+            this.memoTable = new int[numberOfItems + 1,knapsack_weight1+1];
+        }
+
+        public int solve()
+        {
+            int i, j;
+
+            for (i = 1; i < (numberOfItems + 1); i++)
+            {
+
+                for (j = 1; j < (knapsack_weight + 1); j++)
+                {
+
+                    int not_taking_item = memoTable[i - 1,j];
+                    int taking_item = 0;
+
+                    if (weight[i-1] <= knapsack_weight)
+                    {
+                        if (j - weight[i-1] < 0)
+                            taking_item = memoTable[i - 1,j];
+                        else
+                            taking_item = value[i-1] + memoTable[i - 1,j - weight[i-1]];
+                    }
+                    // choosing which is larger
+                    int which_to_take =Math.Max(not_taking_item, taking_item);
+
+                    // storing in the memo table
+                    memoTable[i,j] = which_to_take;
+                }
+            }
+
+            totalValue = memoTable[numberOfItems,knapsack_weight];
+            return totalValue;
+        }
+
+        public String selected_items()
+        {
+
+            /*
+            //Uncomment to print knapsack table
+            for(int i=0; i<=numberOfItems; i++){
+                for(int j=0; j<=knapsack_weight; j++){
+                    System.out.print(memoTable[i][j]+ "\t");
+                }
+                System.out.println();
+            }
+
+            */
+            String test = "";
+            //Checking which item is taken
+            for (int i = numberOfItems, j = knapsack_weight; i > 0; i--)
+            {
+                if (memoTable[i,j] != memoTable[i - 1,j])
+                {
+                    test=test+("Item: " + i + " Selected.\n");
+                    j = j - weight[i-1];
+                    //j represent weight column, since item i is taken
+                    //So subtraction the weight of ith item to reach
+                    //next weight column
+                }
+            }
+            
+            return test;
+        }
+    }
+
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {   if (textBox1.Text == "" || textBox2.Text == "" || textBox3.Text == "" || textBox4.Text == "")
+            {
+                MessageBox.Show("Please Fill all the values!!...");
+
+            }
+            else
+            {
+                int n = Convert.ToInt32(textBox2.Text);
+                int W = Convert.ToInt32(textBox1.Text);
+                String text3 = textBox3.Text;
+                String[] wght = text3.Split(',');
+                int l1 = wght.Length;
+                int i, j = 0;
+                int[] weight = new int[n];
+                string test = "";
+                int Tweight = 0;
+                for (i = 0; i < l1; i++)
+                {
+                    if (wght[i] != " " && j < n)
+                    {
+                        weight[j] = Convert.ToInt32(wght[i]);
+                        Tweight = Tweight + weight[j];
+                        j = j + 1;
+                        
+                    }
+                }
+                String text4 = textBox4.Text;
+                String[] prft = text4.Split(',');
+                int l2 = prft.Length;
+                int[] profit = new int[n];
+                j = 0;
+                for (i = 0; i < l2; i++)
+                {
+                    if (prft[i] != " " && j < n)
+                    {
+                        profit[j] = Convert.ToInt32(prft[i]);
+
+                        j = j + 1;
+
+                    }
+                }
+     
+                if (!radioButton1.Checked && !radioButton2.Checked)
+                {
+                    MessageBox.Show("Please select one type of Filling.");
+                } 
+                else if (radioButton1.Checked)
+                {
+                    int cur_w;
+                    float tot_v;
+                    int maxi;
+                    int[] used=new int[10];
+                    if (Tweight < W)
+                    {
+                        MessageBox.Show("You can add all items to your Bag. Enjoy" + Tweight);
+                    }
+                    else
+                    {
+                        for (i = 0; i < n; ++i)
+                            used[i] = 0; /* I have not used the ith object yet */
+
+                        cur_w = W;
+                        tot_v = 0;
+                        while (cur_w > 0)
+                        { /* while there's still room*/
+                            /* Find the best object */
+                            maxi = -1;
+                            for (i = 0; i < n; ++i)
+                            {
+                                if ((used[i] == 0) && ((maxi == -1) || ((float)profit[i] / weight[i] > (float)profit[maxi] / weight[maxi])))
+                                {
+                                    maxi = i;
+                                }
+                            }
+
+                            used[maxi] = 1; /* mark the maxi-th object as used */
+                            cur_w -= weight[maxi]; /* with the object in the bag, I can carry less */
+                            tot_v += profit[maxi];
+                            if (cur_w >= 0)
+                                test = test + ("\nAdd object " + (maxi + 1) + " (" + profit[maxi] + "$, " + weight[maxi] + "Kg) completely in the bag.");
+                            else
+                            {
+                                int ratio = (int)((1 + (float)cur_w / weight[maxi]) * 100);
+                                test=test+("\nAdd "+ratio+"% ("+ profit[maxi]+"$, "+ weight[maxi] + "Kg) of object "+(maxi + 1)+" in the bag.\n");
+                                tot_v -= profit[maxi];
+                                tot_v += (1 + (float)cur_w / weight[maxi]) * profit[maxi];
+                            }
+                        }
+                        test = test + ("\nFilled the bag with objects worth " + tot_v + "$.\n");
+                        MessageBox.Show(test);
+                        
+                    }
+                }
+                else if (radioButton2.Checked)
+                {
+                    KnapsackDP kdp = new KnapsackDP(W, n, weight, profit);
+                    int prf= kdp.solve();
+                    test= kdp.selected_items();
+                    MessageBox.Show(test+"With Profit: "+prf);
+
+                }
+            }
+
+        }
+    }
+}
